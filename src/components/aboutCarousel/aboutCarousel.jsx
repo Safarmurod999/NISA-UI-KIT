fa8cc5c9501a9732704148f80bf229b0cbb00585
Dilsoor <abdudilsora@gmail.com>
2023-04-06 09:09:59 +0500

    weqr

--- src/components/aboutCarousel/aboutCarousel.jsx
+++ src/components/aboutCarousel/aboutCarousel.jsx
@@ -1,15 +1,68 @@
 import React from "react";
 import "./AboutCarousel.scss";
+import just from "../../assets/About/tinified/qoshtirnoq.svg";
+import left from "../../assets/About/tinified/arrow-left.svg";
+import right from "../../assets/About/tinified/arrow-right.svg";
 
 function AboutCarousel() {
+  const data = [
+    {
+      img: "../../assets/About/tinified/Profil1.png",
+      id: 1,
+    },
+    {
+      img: "../../assets/About/tinified/Profil2.png",
+      id: 2,
+    },
+    {
+      img: "../../assets/About/tinified/Profil3.png",
+      id: 3,
+    },
+  ];
+
+  const sliderLeft = ()=>{
+    
+  }
+  const sliderRight = ()=>{
+
+  }
   return (
     <>
-        <section className="about__header">
-          <button>MEET OUR COMPANY</button>
-          <h1>
-            We’re a people focused <br /> organisation
-          </h1>
-        </section>
+      <section className="carusel">
+        <div className="container">
+          <button>Testimonial </button>
+          <h1>What our client says</h1>
+          <div className="carusel__wrap">
+            {data.map((data, index) => {
+              return (
+                <div key={index} className="carusel__wrap-content">
+                  <div className="carusel__wrap-content_top">
+                    <img src={just} alt="" />
+                  </div>
+                  <h3>{data.id}.TITLE HERE</h3>
+                  <p>
+                    Lorem ipsum dolor sit amet,
+                    <br />
+                    consectetur adipiscing elit. Pretium <br />
+                    consectetur egestas.
+                  </p>
+                  <img src={data.img} alt="" />
+                  <h4>Alex Mex</h4>
+                  <span>CEO/DIRECTOR</span>
+                </div>
+              );
+            })}
+          </div>
+          <div className="carusel__btn">
+            <button onClick={sliderLeft}>
+              <img src={left} alt="left" />
+            </button>
+            <button onClick={sliderRight}>
+              <img src={right} alt="right" />
+            </button>
+          </div>
+        </div>
+      </section>
     </>
   );
 }
--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -2,13 +2,156 @@ import React from "react";
 import "./About.scss";
 import AboutCarousel from "../../components/AboutCarousel/AboutCarousel";
 import Navbar from "../../components/Navbar/Navbar";
+import fokus1 from "../../assets/About/tinified/fokus1.png";
+import fokus2 from "../../assets/About/tinified/fokus2.png";
+import fokus3 from "../../assets/About/tinified/fokus3.png";
+import fokus4 from "../../assets/About/tinified/fokus4.png";
+import fokus5 from "../../assets/About/tinified/fokus5.png";
+import ourTeam1 from "../../assets/About/tinified/ourTeam1.png";
+import ourTeam2 from "../../assets/About/tinified/ourTeam2.png";
+import ourTeam3 from "../../assets/About/tinified/ourTeam3.png";
+import ourTeam4 from "../../assets/About/tinified/ourTeam4.png";
+import ourTeam5 from "../../assets/About/tinified/ourTeam5.png";
+import ourTeam6 from "../../assets/About/tinified/ourTeam6.png";
 
 function About() {
   return (
     <>
-      <Navbar/>
+      <Navbar />
+      <main className="main">
+        <button>MEET OUR COMPANY</button>
+        <h1>
+          We’re a people focused <br /> organisation
+        </h1>
+        <div className="main__wrap">
+          <img src={fokus1} alt="fokus1" />
+          <img src={fokus2} alt="fokus2" />
+          <img src={fokus3} alt="fokus3" />
+          <img src={fokus4} alt="fokus4" />
+          <img src={fokus5} alt="fokus5" />
+        </div>
+        <div className="container">
+          <div className="main__content">
+            <div className="main__content_item">
+              <h2>
+                98<span className="pink">% </span>
+              </h2>
+              <p>Customer satisfaction</p>
+            </div>
+            <div className="main__content_item">
+              <h2>
+                120M<span className="yellow">+</span>
+              </h2>
+              <p>CLIENT RETENTION</p>
+            </div>
+            <div className="main__content_item">
+              <h2>
+                32K<span className="blue">+ </span>
+              </h2>
+              <p>EMAILS PER MONTH</p>
+            </div>
+            <div className="main__content_item">
+              <h2>48M+</h2>
+              <p>MONTHLY CAMAIGNS</p>
+            </div>
+          </div>
+        </div>
+      </main>
       <AboutCarousel />
-      <h1>about</h1>
+      <div className="ourTeam">
+        <div className="container">
+          <button>Our team</button>
+          <h1>
+            People with all the skills you need <br /> are in Nisa!{" "}
+          </h1>
+          <div className="ourTeam__grid">
+            <div className="ourTeam__grid_item">
+              <div className="ourTeam__grid_item-img1">
+                <img src={ourTeam1} alt="ourTeam1" />
+                <button>CEO & FOUNDER</button>
+              </div>
+              <h3>Katie Smith</h3>
+              <p>Lorem ipsum dolor sit amet, consec -tetur adipiscing elit. </p>
+              <div className="ourTeam__grid_item-icons">
+                <i className="uil uil-twitter"></i>
+            
+                <i className="uil uil-instagram-alt"></i>
+                <i className="uil uil-linkedin"></i>
+              </div>
+            </div>
+            <div className="ourTeam__grid_item">
+              <div className="ourTeam__grid_item-img1">
+                <img src={ourTeam2} alt="ourTeam2" />
+                <button>CEO & FOUNDER</button>
+              </div>
+              <h3>Borkat Ali</h3>
+              <p>Lorem ipsum dolor sit amet, consec -tetur adipiscing elit. </p>
+              <div className="ourTeam__grid_item-icons">
+                <i className="uil uil-twitter"></i>
+            
+                <i className="uil uil-instagram-alt"></i>
+                <i className="uil uil-linkedin"></i>
+              </div>
+            </div>
+            <div className="ourTeam__grid_item">
+              <div className="ourTeam__grid_item-img1">
+                <img src={ourTeam3} alt="ourTeam3" />
+                <button>CTO & Co- Founder</button>
+              </div>
+              <h3>Alex Mex</h3>
+              <p>Lorem ipsum dolor sit amet, consec -tetur adipiscing elit. </p>
+              <div className="ourTeam__grid_item-icons">
+                <i className="uil uil-twitter"></i>
+            
+                <i className="uil uil-instagram-alt"></i>
+                <i className="uil uil-linkedin"></i>
+              </div>
+            </div>
+            <div className="ourTeam__grid_item">
+              <div className="ourTeam__grid_item-img1">
+                <img src={ourTeam4} alt="ourTeam4" />
+                <button>VP of Marketing</button>
+              </div>
+              <h3>Tahn Lane</h3>
+              <p>Lorem ipsum dolor sit amet, consec -tetur adipiscing elit. </p>
+              <div className="ourTeam__grid_item-icons">
+                <i className="uil uil-twitter"></i>
+            
+                <i className="uil uil-instagram-alt"></i>
+                <i className="uil uil-linkedin"></i>
+              </div>
+            </div>
+            <div className="ourTeam__grid_item">
+              <div className="ourTeam__grid_item-img1">
+                <img src={ourTeam5} alt="ourTeam5" />
+                <button>VP of product</button>
+              </div>
+              <h3>Maruf Mia</h3>
+              <p>Lorem ipsum dolor sit amet, consec -tetur adipiscing elit. </p>
+              <div className="ourTeam__grid_item-icons">
+                <i className="uil uil-twitter"></i>
+            
+                <i className="uil uil-instagram-alt"></i>
+                <i className="uil uil-linkedin"></i>
+              </div>
+            </div>
+            <div className="ourTeam__grid_item">
+              <div className="ourTeam__grid_item-img1">
+                <img src={ourTeam6} alt="ourTeam6" />
+                <button>VP of Security</button>
+              </div>
+              <h3>Niloy Mex</h3>
+              <p>Lorem ipsum dolor sit amet, consec -tetur adipiscing elit. </p>
+              <div className="ourTeam__grid_item-icons">
+                <i className="uil uil-twitter"></i>
+            
+                <i className="uil uil-instagram-alt"></i>
+                <i className="uil uil-linkedin"></i>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
